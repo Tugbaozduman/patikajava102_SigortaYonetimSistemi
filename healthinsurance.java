c3f78d7b93958a2b1e4d8e1841fc25c37bed95a7
Tugbaozduman <77573771+Tugbaozduman@users.noreply.github.com>
2022-05-23 14:39:07 +0300

    Create healthinsurance.java

---
+++ healthinsurance.java
@@ -0,0 +1,9 @@
+package InsuranceManagementSystem;
+
+public class HealthInsurance extends Insurance{
+    @Override
+    public double calculate() {
+        return 0;
+    }
+}
+© 2022 GitHub, Inc.
